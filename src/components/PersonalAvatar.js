4fa30a885c168c6176bf988739233544083703d0
curieuxio <curieux.mgmt@gmail.com>
2021-11-07 13:49:22 +0100

    v1

--- src/components/PersonalAvatar.js
+++ src/components/PersonalAvatar.js
@@ -53,68 +53,107 @@ export default function PersonalAvatar({ url, onUpload }) {
 
   return (
     <React.Fragment>
+      
+      {/* Artist Banner */}
       {avatarUrl ? (
-        <div className="profile-user-picture">
+      <div className="artist-profile-banner">
+        <div className="artist-profile-banner-content">
           <Avatar
             bg={'#efecf4'}
             size={'300px'}
+            width={'100%'}
+            height={'250px'}
             src={avatarUrl}
             marginBottom={'Opx'}
             alt="Avatar"
             pos={'relative'}
-            _after={{
-              content: '""',
-              w: 25 + "px",
-              h: 25 + "px",
-              bg: '#4fffca',
-              border: '4px solid white',
-              rounded: 'full',
-              pos: 'absolute',
-              bottom: 0,
-              right: 5
-            }}
+            borderRadius={'1.7rem 1.7rem 0 0'}
+            filter={'blur(10px)'}
           />
         </div>
+      </div>
       ) : (
         <Avatar
           bg={'#efecf4'}
-          size={'2xl'}
+          size={'300px'}
+          width={'100%'}
+          height={'250px'}
           src={avatarUrl}
           marginBottom={'Opx'}
           alt="Avatar"
-            marginBottom={'Opx'}
           pos={'relative'}
-          _after={{
-            content: '""',
-            w: 25 + "px",
-            h: 25 + "px",
-            bg: '#4fffca',
-            border: '4px solid white',
-            rounded: 'full',
-            pos: 'absolute',
-            bottom: 0,
-            right: 5
-          }}
+          borderRadius={'1.7rem 1.7rem 0 0'}
+          backdropFilter={'blur(10px)'}
         />
       )}
-        <button className="button-purple upload-profile-picture-button">
-          <label className="buttonLabel" htmlFor="single" >
-            {uploading ? 'Uploading...' : 'Upload'}
-          </label>
-        </button>
+      
+      <div className="artist-profile-main-informations">
+        {/* Artist Profile Picture */}
+        {avatarUrl ? (
+          <div className="profile-user-picture">
+            <Avatar
+              bg={'#efecf4'}
+              width={'160px'}
+              height={'160px'}
+              src={avatarUrl}
+              marginBottom={'Opx'}
+              alt="Avatar"
+              pos={'relative'}
+              _after={{
+                content: '""',
+                w: 25 + "px",
+                h: 25 + "px",
+                bg: '#4fffca',
+                border: '4px solid white',
+                rounded: 'full',
+                pos: 'absolute',
+                bottom: 0,
+                right: 5
+              }}
+            />
+          </div>
+        ) : (
+          <div className="profile-user-picture">
+            <Avatar
+              bg={'#efecf4'}
+              width={'160px'}
+              height={'160px'}
+              src={avatarUrl}
+              marginBottom={'Opx'}
+              alt="Avatar"
+              pos={'relative'}
+              _after={{
+                content: '""',
+                w: 25 + "px",
+                h: 25 + "px",
+                bg: '#4fffca',
+                border: '4px solid white',
+                rounded: 'full',
+                pos: 'absolute',
+                bottom: 0,
+                right: 5
+              }}
+            />
+          </div>
+        )}
+          <button className="button-purple upload-profile-picture-button">
+            <label className="buttonLabel" htmlFor="single" >
+              {uploading ? 'Uploading...' : 'Upload'}
+            </label>
+          </button>
 
-        <input
-          style={{
-            visibility: 'hidden',
-            position: 'absolute'
-          }}
-          type="file"
-          id="single"
-          accept="image/png, image/gif, image/jpeg, image/heic"
-          onChange={uploadAvatar}
-          disabled={uploading}
-        />
-        
+          <input
+            style={{
+              visibility: 'hidden',
+              position: 'absolute'
+            }}
+            type="file"
+            id="single"
+            accept="image/png, image/gif, image/jpeg, image/heic"
+            onChange={uploadAvatar}
+            disabled={uploading}
+          />
+      </div>
       </React.Fragment>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -14,6 +14,9 @@ import InstagramIcon from '../content/images/icons/social-icons/instagram-icon.s
 import TwitterIcon from '../content/images/icons/social-icons/twitter-icon.svg';
 import LinkIcon from '../content/images/icons/link-icon.svg';
 
+// Importing User Avatar //
+import { Avatar } from '@chakra-ui/react';
+
 // Profile Page //
 export default function Account({ session }) {
   const [loading, setLoading] = useState(true);
@@ -136,73 +139,27 @@ export default function Account({ session }) {
                 updateProfile({ ArtistName, ArtistUsername, SpotifyUrl, DeezerUrl, AppleMusicUrl, WebSiteUrl, AvatarUrl: url });
               }}
             />
-
+          </div>
+          
+          {/* USER INFOS */}
+          <div className="artist-profile-block-content">
+            
             {/* USER MAIN INFOS */}
             <h1 className="profile-user-logged-as">You are logged as</h1>
             <h2 className="profile-user-email">{session.user.email}</h2>
-          </div>
             
-          {/* ARTIST NAME INFORMATIONS */}
-          <div className="artist-name-informations">
-          {/* Artist Name */}
-          <div className="link-input">
-                <p className="form-input-category">Artist Name</p>
-                <div className="artist-card-socials-block">
-                  <a><img className="artist-card-socials-icons" src={ProfileIcon} alt="Link Logo"/></a>
-                  <input className="profile-link-input"
-                type={'text'}
-                value={ArtistName || ''}
-                onChange={e => setArtistName(e.target.value)}
-                placeholder={ArtistName || 'Name'}
-                color={useColorModeValue('gray.800', 'gray.200')}
-                bg={useColorModeValue('gray.100', 'gray.600')}
-                rounded={'full'}
-                border={0}
-                _focus={{
-                  bg: useColorModeValue('gray.200', 'gray.800'),
-                  outline: 'none'
-                }}
-              />
-            </div>
-          </div>
-
-          {/* Artist Username */}
-          <div className="link-input">
-                <p className="form-input-category">Username</p>
-                <div className="artist-card-socials-block">
-                  <a href={ArtistUsername} target="_blank" className="artist-card-socials-icons-a"><img className="artist-card-socials-icons" src={LinkIcon} alt="Link Logo"/></a>
-                  <input className="profile-link-input"
-                type={'text'}
-                value={ArtistUsername || ''}
-                onChange={e => setArtistUsername(e.target.value)}
-                placeholder={ArtistUsername || 'Username'}
-                color={useColorModeValue('gray.800', 'gray.200')}
-                bg={useColorModeValue('gray.100', 'gray.600')}
-                rounded={'full'}
-                border={0}
-                _focus={{
-                  bg: useColorModeValue('gray.200', 'gray.800'),
-                  outline: 'none'
-                }}
-              />
-            </div>
-          </div>
-          </div>
-
-            
-          {/* ARTIST LINKS */}
-          <div className="artist-links-block">
-
-            {/* Spotify Link */}
+            {/* ARTIST NAME INFORMATIONS */}
+            <div className="artist-name-informations">
+            {/* Artist Name */}
             <div className="link-input">
-              <p className="form-input-category">Spotify</p>
-              <div className="artist-card-socials-block">
-                <a href={SpotifyUrl} target="_blank" className="artist-card-socials-icons-a"><img className="artist-card-socials-icons" src={SpotifyIcon} alt="Spotify Logo"/></a>
-                <input className="profile-link-input"
+                  <p className="form-input-category">Artist Name</p>
+                  <div className="artist-card-socials-block">
+                    <a><img className="artist-card-socials-icons" src={ProfileIcon} alt="Link Logo"/></a>
+                    <input className="profile-link-input"
                   type={'text'}
-                  value={SpotifyUrl || ''}
-                  onChange={e => setSpotifyUrl(e.target.value)}
-                  placeholder={SpotifyUrl || 'https://open.spotify.com'}
+                  value={ArtistName || ''}
+                  onChange={e => setArtistName(e.target.value)}
+                  placeholder={ArtistName || 'Name'}
                   color={useColorModeValue('gray.800', 'gray.200')}
                   bg={useColorModeValue('gray.100', 'gray.600')}
                   rounded={'full'}
@@ -214,109 +171,158 @@ export default function Account({ session }) {
                 />
               </div>
             </div>
+
+            {/* Artist Username */}
+            <div className="link-input">
+                  <p className="form-input-category">Username</p>
+                  <div className="artist-card-socials-block">
+                    <a href={ArtistUsername} target="_blank" className="artist-card-socials-icons-a"><img className="artist-card-socials-icons" src={LinkIcon} alt="Link Logo"/></a>
+                    <input className="profile-link-input"
+                  type={'text'}
+                  value={ArtistUsername || ''}
+                  onChange={e => setArtistUsername(e.target.value)}
+                  placeholder={ArtistUsername || 'Username'}
+                  color={useColorModeValue('gray.800', 'gray.200')}
+                  bg={useColorModeValue('gray.100', 'gray.600')}
+                  rounded={'full'}
+                  border={0}
+                  _focus={{
+                    bg: useColorModeValue('gray.200', 'gray.800'),
+                    outline: 'none'
+                  }}
+                />
+              </div>
+            </div>
+            </div>
+
+              
+            {/* ARTIST LINKS */}
+            <div className="artist-links-block">
+
+              {/* Spotify Link */}
+              <div className="link-input">
+                <p className="form-input-category">Spotify</p>
+                <div className="artist-card-socials-block">
+                  <a href={SpotifyUrl} target="_blank" className="artist-card-socials-icons-a"><img className="artist-card-socials-icons" src={SpotifyIcon} alt="Spotify Logo"/></a>
+                  <input className="profile-link-input"
+                    type={'text'}
+                    value={SpotifyUrl || ''}
+                    onChange={e => setSpotifyUrl(e.target.value)}
+                    placeholder={SpotifyUrl || 'https://open.spotify.com'}
+                    color={useColorModeValue('gray.800', 'gray.200')}
+                    bg={useColorModeValue('gray.100', 'gray.600')}
+                    rounded={'full'}
+                    border={0}
+                    _focus={{
+                      bg: useColorModeValue('gray.200', 'gray.800'),
+                      outline: 'none'
+                    }}
+                  />
+                </div>
+              </div>
+              
+                {/* Deezer Link */}
+                <div className="link-input">
+                  <p className="form-input-category">Deezer</p>
+                  <div className="artist-card-socials-block">
+                    <a href={DeezerUrl} target="_blank" className="artist-card-socials-icons-a"><img className="artist-card-socials-icons" src={DeezerIcon} alt="Deezer Logo"/></a>
+                    <input className="profile-link-input"
+                    type={'text'}
+                    value={DeezerUrl || ''}
+                    onChange={e => setDeezerUrl(e.target.value)}
+                    placeholder={DeezerUrl || 'https://deezer.com'}
+                    color={useColorModeValue('gray.800', 'gray.200')}
+                    bg={useColorModeValue('gray.100', 'gray.600')}
+                    rounded={'full'}
+                    border={0}
+                    _focus={{
+                      bg: useColorModeValue('gray.200', 'gray.800'),
+                      outline: 'none'
+                    }}
+                  />
+                  </div>
+                </div>
+
+                {/* Apple Music Link */}
+                <div className="link-input">
+                  <p className="form-input-category">Apple Music</p>
+                  <div className="artist-card-socials-block">
+                    <a href={AppleMusicUrl} target="_blank" className="artist-card-socials-icons-a"><img className="artist-card-socials-icons artist-card-socials-icons-apple" src={AppleMusicIcon} alt="Apple Music Logo"/></a>
+                    <input className="profile-link-input"
+                    type={'text'}
+                    value={AppleMusicUrl || ''}
+                    onChange={e => setAppleMusicUrl(e.target.value)}
+                    placeholder={AppleMusicUrl || 'https://music.apple.com'}
+                    color={useColorModeValue('gray.800', 'gray.200')}
+                    bg={useColorModeValue('gray.100', 'gray.600')}
+                    rounded={'full'}
+                    border={0}
+                    _focus={{
+                      bg: useColorModeValue('gray.200', 'gray.800'),
+                      outline: 'none'
+                    }}
+                  />
+                  </div>
+                </div>
+
+                {/* WebSite Link */}
+                <div className="link-input">
+                  <p className="form-input-category">Web Site</p>
+                  <div className="artist-card-socials-block">
+                    <a href={WebSiteUrl} target="_blank" className="artist-card-socials-icons-a"><img className="artist-card-socials-icons" src={LinkIcon} alt="Link Logo"/></a>
+                    <input className="profile-link-input"
+                    type={'text'}
+                    value={WebSiteUrl || ''}
+                    onChange={e => setWebSiteUrl(e.target.value)}
+                    placeholder={WebSiteUrl || 'https://artistwebsite.com'}
+                    color={useColorModeValue('gray.800', 'gray.200')}
+                    bg={useColorModeValue('gray.100', 'gray.600')}
+                    rounded={'full'}
+                    border={0}
+                    _focus={{
+                      bg: useColorModeValue('gray.200', 'gray.800'),
+                      outline: 'none'
+                    }}
+                  />
+                  </div>
+                </div>
+            </div>
+
+            {/* Buttons Part */}
+            <div className="profile-bottom-buttons">
+
+              {/* Update Button */}
+              <button className="button-purple update-profile-button"
+                isLoading={loading}
+                loadingText="Updating ..."
+                onClick={() => updateProfile({ ArtistName, ArtistUsername, SpotifyUrl, DeezerUrl, AppleMusicUrl, WebSiteUrl, AvatarUrl })}
+                flex={1}
+                fontSize={'sm'}
+                rounded={'full'}
+                bg={'#7000FF'}
+                color={'white'}
+                boxShadow={'0 5px 20px 0px rgb(72 187 120 / 43%)'}
+                _hover={{
+                  bg: '#5800CC'
+                }}
+                _focus={{
+                  bg: '#5800CC'
+                }}>
+                {loading ? 'Updating...' : 'Update'}
+              </button>
             
-              {/* Deezer Link */}
-              <div className="link-input">
-                <p className="form-input-category">Deezer</p>
-                <div className="artist-card-socials-block">
-                  <a href={DeezerUrl} target="_blank" className="artist-card-socials-icons-a"><img className="artist-card-socials-icons" src={DeezerIcon} alt="Deezer Logo"/></a>
-                  <input className="profile-link-input"
-                  type={'text'}
-                  value={DeezerUrl || ''}
-                  onChange={e => setDeezerUrl(e.target.value)}
-                  placeholder={DeezerUrl || 'https://deezer.com'}
-                  color={useColorModeValue('gray.800', 'gray.200')}
-                  bg={useColorModeValue('gray.100', 'gray.600')}
-                  rounded={'full'}
-                  border={0}
-                  _focus={{
-                    bg: useColorModeValue('gray.200', 'gray.800'),
-                    outline: 'none'
-                  }}
-                />
-                </div>
-              </div>
-
-              {/* Apple Music Link */}
-              <div className="link-input">
-                <p className="form-input-category">Apple Music</p>
-                <div className="artist-card-socials-block">
-                  <a href={AppleMusicUrl} target="_blank" className="artist-card-socials-icons-a"><img className="artist-card-socials-icons artist-card-socials-icons-apple" src={AppleMusicIcon} alt="Apple Music Logo"/></a>
-                  <input className="profile-link-input"
-                  type={'text'}
-                  value={AppleMusicUrl || ''}
-                  onChange={e => setAppleMusicUrl(e.target.value)}
-                  placeholder={AppleMusicUrl || 'https://music.apple.com'}
-                  color={useColorModeValue('gray.800', 'gray.200')}
-                  bg={useColorModeValue('gray.100', 'gray.600')}
-                  rounded={'full'}
-                  border={0}
-                  _focus={{
-                    bg: useColorModeValue('gray.200', 'gray.800'),
-                    outline: 'none'
-                  }}
-                />
-                </div>
-              </div>
-
-              {/* WebSite Link */}
-              <div className="link-input">
-                <p className="form-input-category">Web Site</p>
-                <div className="artist-card-socials-block">
-                  <a href={WebSiteUrl} target="_blank" className="artist-card-socials-icons-a"><img className="artist-card-socials-icons" src={LinkIcon} alt="Link Logo"/></a>
-                  <input className="profile-link-input"
-                  type={'text'}
-                  value={WebSiteUrl || ''}
-                  onChange={e => setWebSiteUrl(e.target.value)}
-                  placeholder={WebSiteUrl || 'https://artistwebsite.com'}
-                  color={useColorModeValue('gray.800', 'gray.200')}
-                  bg={useColorModeValue('gray.100', 'gray.600')}
-                  rounded={'full'}
-                  border={0}
-                  _focus={{
-                    bg: useColorModeValue('gray.200', 'gray.800'),
-                    outline: 'none'
-                  }}
-                />
-                </div>
-              </div>
-          </div>
-
-          {/* Buttons Part */}
-          <div className="profile-bottom-buttons">
-
-            {/* Update Button */}
-            <button className="button-purple update-profile-button"
-              isLoading={loading}
-              loadingText="Updating ..."
-              onClick={() => updateProfile({ ArtistName, ArtistUsername, SpotifyUrl, DeezerUrl, AppleMusicUrl, WebSiteUrl, AvatarUrl })}
-              flex={1}
-              fontSize={'sm'}
-              rounded={'full'}
-              bg={'#7000FF'}
-              color={'white'}
-              boxShadow={'0 5px 20px 0px rgb(72 187 120 / 43%)'}
-              _hover={{
-                bg: '#5800CC'
-              }}
-              _focus={{
-                bg: '#5800CC'
-              }}>
-              {loading ? 'Updating...' : 'Update'}
-            </button>
-          
-            {/* Logout Button */}
-            <button className="button-grey logout-button"
-              onClick={() => supabase.auth.signOut()}
-              flex={1}
-              fontSize={'sm'}
-              rounded={'full'}
-              _focus={{
-                bg: 'gray.200'
-              }}>
-              Logout
-            </button>
-          </div>
+              {/* Logout Button */}
+              <button className="button-grey logout-button"
+                onClick={() => supabase.auth.signOut()}
+                flex={1}
+                fontSize={'sm'}
+                rounded={'full'}
+                _focus={{
+                  bg: 'gray.200'
+                }}>
+                Logout
+              </button>
+            </div>
 
           {/* FOOTER PART */}
           <div className="profile-footer">
@@ -325,6 +331,7 @@ export default function Account({ session }) {
 
         </div>
         </div>
+      </div>
       </section>
   );
 }
